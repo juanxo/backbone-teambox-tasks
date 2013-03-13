e249604e702e71c441fb2e0e4bd8639aeb656250
Juan Guerrero <juanxo1990@gmail.com>
2013-03-14 00:44:42 +0100

    Move oauth.OnSuccess to router

--- app/assets/javascripts/routers/app-router.js
+++ app/assets/javascripts/routers/app-router.js
@@ -8,6 +8,20 @@ Juanxo.Routers.AppRouter = Backbone.Router.extend({
     var teamboxOAuth = new Backbone.OAuth(Backbone.OAuth.configs.Teambox);
     teamboxOAuth.auth();
 
+    teamboxOAuth.onSuccess = function(params) {
+      if ( !(params && params.access_token) ) {
+        alert('Corrupted response');
+      }
+
+      // Sets the access token to use in all the fetch requests
+      Juanxo.Config.accessToken = params.access_token;
+      var taskCollection = new Juanxo.Collections.Tasks();
+      var taskCollectionView = new Juanxo.Views.TaskCollectionView({
+        collection: taskCollection
+      });
+      taskCollection.fetch();
+
+    }
   }
 
 });
