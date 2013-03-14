72b0f757f3c1986517f199103c9b15152f7d7898
Juan Guerrero <juanxo1990@gmail.com>
2013-03-14 01:34:15 +0100

    Add sort of Backbone bootstrap with deferreds

--- app/assets/javascripts/routers/app-router.js
+++ app/assets/javascripts/routers/app-router.js
@@ -15,19 +15,29 @@ Juanxo.Routers.AppRouter = Backbone.Router.extend({
 
       // Sets the access token to use in all the fetch requests
       Juanxo.Config.accessToken = params.access_token;
-      var taskCollection = new Juanxo.Collections.Tasks();
-      var taskCollectionView = new Juanxo.Views.TaskCollectionView({
-        collection: taskCollection
-      });
-      taskCollection.fetch();
 
-    }
+      var taskCollectionView = new Juanxo.Views.TaskCollectionView({
+        collection: Juanxo.collections.tasks
+      });
+     }
   }
 
 });
 
 var appRouter = new Juanxo.Routers.AppRouter();
 
-Backbone.history.start();
+
+// Fetch all models and start the app after they've
+// been all loaded.
+// Kinda of backbone bootstrap but with external api ;)
+Juanxo.collections = {
+  tasks: new Juanxo.Collections.Tasks(),
+  users: new Juanxo.Collections.Users()
+};
+
+$.when(
+    Juanxo.collections.tasks.fetch(),
+    Juanxo.collections.users.fetch()
+).done(function(){Backbone.history.start()});
 
 }).call(this);
