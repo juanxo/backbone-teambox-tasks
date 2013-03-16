ca5459736fec62d2e16bed2d338b6b1db199112c
Juan Guerrero <juanxo1990@gmail.com>
2013-03-16 04:10:49 +0100

    Change fetch order
    I was fetching collections before getting oauth acess. Silly me O.o

--- app/assets/javascripts/routers/app-router.js
+++ app/assets/javascripts/routers/app-router.js
@@ -5,39 +5,44 @@ Juanxo.Routers.AppRouter = Backbone.Router.extend({
   },
 
   index: function() {
-    var teamboxOAuth = new Backbone.OAuth(Backbone.OAuth.configs.Teambox);
-    teamboxOAuth.auth();
+    var taskCollectionView = new Juanxo.Views.TaskCollectionView({
+      collection: Juanxo.collections.tasks
+    });
 
-    teamboxOAuth.onSuccess = function(params) {
-      if ( !(params && params.access_token) ) {
-        alert('Corrupted response');
-      }
-
-      // Sets the access token to use in all the fetch requests
-      Juanxo.Config.accessToken = params.access_token;
-
-      var taskCollectionView = new Juanxo.Views.TaskCollectionView({
-        collection: Juanxo.collections.tasks
-      });
-     }
   }
 
 });
 
-var appRouter = new Juanxo.Routers.AppRouter();
+$(document).ready( function() {
+  var teamboxOAuth = new Backbone.OAuth(Backbone.OAuth.configs.Teambox);
+
+  teamboxOAuth.onSuccess = function(params) {
+
+    if ( !(params && params.access_token) ) {
+      alert('Corrupted response');
+    } else {
+      var appRouter = new Juanxo.Routers.AppRouter();
 
 
-// Fetch all models and start the app after they've
-// been all loaded.
-// Kinda of backbone bootstrap but with external api ;)
-Juanxo.collections = {
-  tasks: new Juanxo.Collections.Tasks(),
-  users: new Juanxo.Collections.Users()
-};
+      // Fetch all models and start the app after they've
+      // been all loaded.
+      // Kinda of backbone bootstrap but with external api ;)
+      Juanxo.collections = {
+        tasks: new Juanxo.Collections.Tasks(),
+        users: new Juanxo.Collections.Users()
+      };
 
-$.when(
-    Juanxo.collections.tasks.fetch(),
-    Juanxo.collections.users.fetch()
-).done(function(){Backbone.history.start()});
+      // Sets the access token to use in all the fetch requests
+      Juanxo.Config.accessToken = params.access_token;
+      $.when(
+          Juanxo.collections.tasks.fetch(),
+          Juanxo.collections.users.fetch()
+          ).done(function(){Backbone.history.start()});
+    }
+
+  }
+
+  teamboxOAuth.auth();
+});
 
 }).call(this);
