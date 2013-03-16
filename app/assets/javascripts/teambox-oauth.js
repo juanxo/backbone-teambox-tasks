fdcbf3d79eb1aa671c99efa345b813780d1f1837
Juan Guerrero <juanxo1990@gmail.com>
2013-03-16 04:04:38 +0100

    Get out of localhost

--- app/assets/javascripts/teambox-oauth.js
+++ app/assets/javascripts/teambox-oauth.js
@@ -2,7 +2,7 @@
 _.extend(Backbone.OAuth.configs.Teambox, {
 
   client_id: '0BFiizh27YRyCmTTRsh5zxMe9quXSjqVWffNdFB5',
-  redirect_url: 'http://localhost:3000/oauth.html',
+  redirect_url: 'http://teambox-tasks.herokuapp.com/oauth.html',
 
   onError: function(params) {
     alert('Something went wrong requesting Teambox access');
